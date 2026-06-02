ff645d4ef86d4bb35d193ebab35fa6028c114fa4
Ryrybros <ryadgourine1@gmail.com>
2026-06-02 23:24:53 +0200

    ntm

--- tuto_assembly/test.asm
+++ tuto_assembly/test.asm
@@ -4,53 +4,23 @@ extern printf; e.g stdio.h
     asm_ret_msg: db 10,"Program executed successfully." ,10 , 10, 0
     asm_int_prtr : db "%d" , 0
     argv : dq 0
-    x : dq 0
+    a : dq 0
    global main
     section .text
-            ;This is a function
-            hallo:                    ;Name of func
-
-            push rbp
-            mov rbp, rsp
-            
-            mov rax, 0
-                    mov rdi , rax
-                
-         
-            mov rax, [x]
-            push rax
-            mov rax, [x]
-            pop rbx
-            imul  rax, rbx
-            
-        
-        mov rsp, rbp           
-        pop rbp     
-        ret
-            
-            
             main:
             push rbp            
             mov rbp, rsp
             mov [argv], rsi
             
             
-                    mov rax, 10
-                    mov qword [x] , rax
-                ;This is a function call
-mov rax, 0
-                push rdi
-                mov rdi , rax
-                
-
-call hallo;end_func_call
-
-                pop rdi
-                
-                    mov rdi, asm_int_prtr
+                    push rax
+                    mov rax, 0x0A22646E6569724620794D206F6C6C654822  
+                    push rax
+                    mov rdi, rsp
                     mov rsi, rax
                     xor rax, rax
                     call printf
+                    add rsp, 16
                 
             
             mov rdi, asm_ret_msg
